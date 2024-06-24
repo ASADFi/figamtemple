8fe3b65bfe958e02c9f5ef725323a5861846b8a3
Asad Meer <meerasads636@gmail.com>
2024-06-24 20:01:10 +0500

    sql API route updated.

--- src/app/api/sql/[id]/route.ts
+++ src/app/api/sql/[id]/route.ts
@@ -89,33 +89,13 @@ export async function POST(request: Request) {
 
 
 export async function GET(request:Request ,context:any) {
-  const {params} =context;
-  console.log(params);
-  try {
-    const result= await new Promise((resolve,reject)=>{
-      con.query(`SELECT * FROM users where id=?`,[params.id],(err:any ,result:[])=>{
-           if(err){
-            reject(err);
-           }
-           else{
-            resolve(result);
-            
-           }
-      });
-    });
-    console.log("result", result);
-    return NextResponse.json({
-    result
-    }, { status: 201 });
-  } 
-  catch (error) {
-
-
-     return NextResponse.json(
-      { message: error},
-    
-    
-    )
-  }
+  return NextResponse.json({
+    products: [
+      {
+        id: 1,
+        name: "Banana",
+      },
+    ],
+  });
   
   }
