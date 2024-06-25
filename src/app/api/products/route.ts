75a4fe93d2948c55370a696ffed03379e7e8e183
Asad Meer <meerasads636@gmail.com>
2024-06-25 13:08:51 +0500

    simple router

--- src/app/api/products/route.ts
+++ src/app/api/products/route.ts
@@ -1,12 +1,47 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.json({
-    products: [
-      {
-        id: 1,
-        name: "Banana",
-      },
-    ],
-  });
-}
+// export async function GET() {
+//   return NextResponse.json({
+//     products: [
+//       {
+//         id: 1,
+//         name: "Banana",
+//       },
+//     ],
+//   });
+// }
+
+import { NextResponse ,NextRequest } from 'next/server';
+import con from "@/db";
+
+export async function GET(request:Request ,context:any) {
+  const {params} =context;
+  console.log(params);
+  try {
+    const result= await new Promise((resolve,reject)=>{
+      con.query(`SELECT * FROM users where id=2`,(err:any ,result:[])=>{
+           if(err){
+            reject(err);
+           }
+           else{
+            resolve(result);
+            
+           }
+      });
+    });
+    console.log("result", result);
+    return NextResponse.json({
+    result
+    }, { status: 201 });
+  } 
+  catch (error) {
+
+
+     return NextResponse.json(
+      { message: error},
+    
+    
+    )
+  }
+  
+  }
